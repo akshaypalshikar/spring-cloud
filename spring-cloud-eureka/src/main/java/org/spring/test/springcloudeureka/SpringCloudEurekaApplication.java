2d006f4fe6a95fa9e6d985dbcb0a7d723d3997db
akshaypalshikar <aksmakesit@gmail.com>
2017-12-25 01:04:05 -0600

    added files

---
+++ spring-cloud-eureka/src/main/java/org/spring/test/springcloudeureka/SpringCloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package org.spring.test.springcloudeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
