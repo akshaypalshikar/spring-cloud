0204f51124ae5d2b29476c935f5a0c11facc3203
akshaypalshikar <aksmakesit@gmail.com>
2017-12-25 12:19:10 -0600

    package name changed

--- spring-cloud-service/src/main/java/org/aks/cloud/service/SpringCloudServiceApplication.java
+++ spring-cloud-service/src/main/java/org/aks/cloud/service/SpringCloudServiceApplication.java
@@ -1,4 +1,4 @@
-package org.spring.test.springcloudservice;
+package org.aks.cloud.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
